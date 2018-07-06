22f5f35c4194293759c5785e6d22a8fbc7a23e15
Aaron Chen <chen_dong85@foxmail.com>
2018-07-06 11:08:17 +0800

    add the missing mapTo

--- chapter-07/filter/src/debounceTime/random.js
+++ chapter-07/filter/src/debounceTime/random.js
@@ -4,6 +4,7 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/concat';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/mapTo';
 
 const source$ = Observable.interval(500).take(2).mapTo('A')
   .concat(Observable.interval(1000).take(3).mapTo('B'))
